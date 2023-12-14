c7c7536a3739bf8052c673391d2bb46d213f99bf
Sahithi <1rn21ai094.sahithi@gmail.com>
2023-12-14 17:38:20 +0530

    linear regression

---
+++ src/preprocessing.py
@@ -0,0 +1,285 @@
+import os
+import sys
+
+from src.exception import CustomException
+from src.logger import logging
+import pandas as pd
+import numpy as np
+
+from sklearn.model_selection import train_test_split
+from dataclasses import dataclass
+
+from sklearn.compose import ColumnTransformer
+from sklearn.impute import SimpleImputer
+from sklearn.pipeline import Pipeline
+
+
+from src.utils import save_object
+from sklearn.linear_model import LinearRegression
+from sklearn.preprocessing import PolynomialFeatures
+from sklearn.model_selection import GridSearchCV
+from sklearn.preprocessing import MinMaxScaler
+from sklearn.metrics import r2_score
+from sklearn.metrics import mean_absolute_error
+from sklearn.metrics import mean_squared_error
+
+
+@dataclass
+class DataIngestionConfig:
+    train_data_path: str = os.path.join("artifacts", "train.csv")
+    test_data_path: str = os.path.join("artifacts", "test.csv")
+    raw_data_path: str = os.path.join("artifacts", "data.csv")
+
+
+class DataIngestion:
+    def __init__(self):
+        self.ingestion_config = DataIngestionConfig()
+
+
+class DataIngestion:
+    def __init__(self):
+        self.ingestion_config = DataIngestionConfig()
+
+    def initiate_data_ingestion(self):
+        logging.info("Entered the data ingestion method or component")
+        try:
+            column_names = [
+                "CRIM",
+                "ZN",
+                "INDUS",
+                "CHAS",
+                "NOX",
+                "RM",
+                "AGE",
+                "DIS",
+                "RAD",
+                "TAX",
+                "PTRATIO",
+                "B",
+                "LSTAT",
+                "MEDV",
+            ]
+
+            df = pd.read_csv(
+                "src\\data\\housing.csv",
+                delimiter=r"\s+",
+                names=column_names,
+            )
+            logging.info("Read the dataset as dataframe")
+
+            os.makedirs(
+                os.path.dirname(self.ingestion_config.train_data_path), exist_ok=True
+            )
+
+            df.to_csv(
+                self.ingestion_config.raw_data_path, index=False, header=True, sep=","
+            )
+
+            logging.info("Train test split initiated")
+            train_set, test_set = train_test_split(df, test_size=0.2, random_state=42)
+
+            train_set.to_csv(
+                self.ingestion_config.train_data_path, index=False, header=True, sep=","
+            )
+            test_set.to_csv(
+                self.ingestion_config.test_data_path, index=False, header=True, sep=","
+            )
+
+            logging.info("Ingestion of the data is completed")
+
+            return (
+                self.ingestion_config.train_data_path,
+                self.ingestion_config.test_data_path,
+            )
+        except Exception as e:
+            raise CustomException(e, sys)
+
+
+class DataTransformationConfig:
+    preprocessor_obj_file_path = os.path.join("artifacts", "preprocessor.pkl")
+
+
+class DataTransformation:
+    def __init__(self):
+        self.data_transformation_config = DataTransformationConfig()
+
+    def get_data_transformer_object(self):
+        """
+        This function is used to perform data Transformation
+
+        """
+        try:
+            numerical_columns = [
+                "CRIM",
+                "ZN",
+                "INDUS",
+                "CHAS",
+                "NOX",
+                "RM",
+                "AGE",
+                "DIS",
+                "RAD",
+                "TAX",
+                "PTRATIO",
+                "B",
+                "LSTAT",
+            ]
+
+            num_pipeline = Pipeline(
+                steps=[
+                    ("imputer", SimpleImputer(strategy="median")),
+                    ("scaler", MinMaxScaler()),
+                ]
+            )
+
+            # logging.info(f"Numerical columns: {numerical_columns}")
+            poly_features = PolynomialFeatures(degree=2, include_bias=False)
+
+            preprocessor = ColumnTransformer(
+                [
+                    ("num_pipeline", num_pipeline, numerical_columns),
+                    ("poly_features", poly_features, numerical_columns),
+                ],
+            )
+            return preprocessor
+
+        except Exception as e:
+            raise CustomException(e, sys)
+
+    def initiate_data_transformation(self, train_path, test_path):
+        try:
+            train_df = pd.read_csv(train_path)
+            test_df = pd.read_csv(test_path)
+
+            logging.info("Read train and test data completed")
+
+            logging.info("Obtaining preprocessing object")
+
+            preprocessing_obj = self.get_data_transformer_object()
+
+            target_column_name = "MEDV"
+            numerical_columns = [
+                "CRIM",
+                "ZN",
+                "INDUS",
+                "CHAS",
+                "NOX",
+                "RM",
+                "AGE",
+                "DIS",
+                "RAD",
+                "TAX",
+                "PTRATIO",
+                "B",
+                "LSTAT",
+            ]
+
+            input_feature_train_df = train_df.drop(columns=[target_column_name], axis=1)
+            target_feature_train_df = train_df[target_column_name]
+
+            input_feature_test_df = test_df.drop(columns=[target_column_name], axis=1)
+            target_feature_test_df = test_df[target_column_name]
+
+            logging.info(
+                f"Applying preprocessing object on training dataframe and testing dataframe."
+            )
+
+            input_feature_train_arr = preprocessing_obj.fit_transform(
+                input_feature_train_df
+            )
+            input_feature_test_arr = preprocessing_obj.transform(input_feature_test_df)
+
+            train_arr = np.c_[
+                input_feature_train_arr, np.array(target_feature_train_df)
+            ]
+            test_arr = np.c_[input_feature_test_arr, np.array(target_feature_test_df)]
+
+            logging.info(f"Saved preprocessing object.")
+
+            save_object(
+                file_path=self.data_transformation_config.preprocessor_obj_file_path,
+                obj=preprocessing_obj,
+            )
+
+            return (
+                train_arr,
+                test_arr,
+                self.data_transformation_config.preprocessor_obj_file_path,
+            )
+        except Exception as e:
+            raise CustomException(e, sys)
+
+
+class ModelTrainerConfig:
+    trained_model_file_path = os.path.join("artifacts", "model.pkl")
+
+
+class ModelTrainer:
+    def __init__(self):
+        self.model_trainer_config = ModelTrainerConfig()
+
+    def initiate_model_trainer(self, train_array, test_array):
+        try:
+            logging.info("Split training and test input data")
+            X_train, y_train, X_test, y_test = (
+                train_array[:, :-1],
+                train_array[:, -1],
+                test_array[:, :-1],
+                test_array[:, -1],
+            )
+            models = [
+                LinearRegression(),
+            ]
+            params = [
+                {
+                    "fit_intercept": [True, False],
+                },
+            ]
+            # Set up GridSearchCV
+            grid_search = GridSearchCV(models[0], params, cv=5)
+
+            # Fit the model with cross-validation and hyperparameter tuning
+            grid_search.fit(X_train, y_train)
+
+            models = grid_search.best_estimator_
+            y_train_pred = models.predict(X_train)
+
+            y_test_pred = models.predict(X_test)
+
+            train_model_score = r2_score(y_train, y_train_pred)
+
+            test_model_score = r2_score(y_test, y_test_pred)
+
+            if test_model_score < 0.6:
+                raise CustomException("No best model found")
+            logging.info(f"Best found model on both training and testing dataset")
+
+            save_object(
+                file_path=self.model_trainer_config.trained_model_file_path,
+                obj=models,
+            )
+
+            predicted = models.predict(X_test)
+            r2_square = r2_score(y_test, predicted)
+            mae_score = mean_absolute_error(y_test, predicted)
+            mse_score = mean_squared_error(y_test, predicted)
+            return (
+                f"r2_sqaure: {r2_square}",
+                f"Mean Absolute Error(MAE): {mae_score}",
+                f"Mean Squared Error(MSE): {mse_score}",
+            )
+
+        except Exception as e:
+            raise CustomException(e, sys)
+
+
+if __name__ == "__main__":
+    obj = DataIngestion()
+    train_data, test_data = obj.initiate_data_ingestion()
+
+    data_transformation = DataTransformation()
+    train_arr, test_arr, _ = data_transformation.initiate_data_transformation(
+        train_data, test_data
+    )
+    modeltrainer = ModelTrainer()
+    print(modeltrainer.initiate_model_trainer(train_arr, test_arr))
